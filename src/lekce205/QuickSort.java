458d3dae83856cf411815e0add09bd3d00e2b265
Vojtěch Holcman <vojtech.holcman.s@gyarab.cz>
2026-03-20 10:02:14 +0100

    Modified: QuickSort

--- src/lekce205/QuickSort.java
+++ src/lekce205/QuickSort.java
@@ -13,7 +13,8 @@ import java.util.Random;
  * @author vojtech.holcman.s
  */
 public class QuickSort {
-        public static void main(String[] args) {
+
+    public static void main(String[] args) {
 //        int a[] = {100, 1, 22, 3};
         double a[] = new double[100];
         Random r = new Random(0);
@@ -24,11 +25,49 @@ public class QuickSort {
         sort(a);
         System.out.println(Arrays.toString(a));
     }
-    
+
     public static void sort(double data[]) {
         sort(data, 0, data.length);
     }
-    private static void sort(double data[], int left, int right){
-        
+
+    private static void sort(double data[], int left, int right) {
+        if (right - left < 1) {
+            return;
+        }
+        if (right - left == 2) {
+            if (data[left] > data[right - 1]) {
+                double tmp = data[left];
+                data[left] = data[right - 1];
+                data[right - 1] = tmp;
+            }
+            return;
+        }
+        double pivot = data[(left + right) / 2];
+
+        int i = left;
+        int j = right - 1;
+
+        while (i <= j) {
+            while (data[i] < pivot) {
+                i++;
+            }
+
+            while (data[j] > pivot) {
+                j--;
+            }
+
+            if (i <= j) {
+                double tmp = data[i];
+                data[i] = data[j];
+                data[j] = tmp;
+
+                i++;
+                j--;
+            }
+        }
+
+        sort(data, left, j + 1);
+        sort(data, i, right);
     }
 }
+
