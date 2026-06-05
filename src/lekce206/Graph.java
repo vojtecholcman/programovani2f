beab7ae9c07834ec358bacd589ad69caffdca95d
Vojtěch Holcman <vojtech.holcman.s@gyarab.cz>
2026-06-05 10:05:50 +0200

    added Graph all

--- src/lekce206/Graph.java
+++ src/lekce206/Graph.java
@@ -15,12 +15,14 @@ import java.util.Set;
 public class Graph<E> {
 
     public static void main(String[] args) {
-        Graph<String> g = new Graph<>();
+        Graph<String> g = new Graph<>(true);
+
         Node<String> a = g.add("A");
         Node<String> b = g.add("B");
         Node<String> c = g.add("C");
         Node<String> d = g.add("D");
         Node<String> e = g.add("E");
+
         g.link(a, b);
         g.link(a, c);
         g.link(b, c);
@@ -28,7 +30,17 @@ public class Graph<E> {
         g.link(c, d);
         g.link(d, e);
         g.link(e, d);
-        System.out.println(a);
+
+        for (Node<String> node : g.nodes) {
+            System.out.println(node);
+        }
+    }
+
+    private final boolean oriented;
+    private final Set<Node<E>> nodes = new HashSet<>();
+
+    public Graph(boolean oriented) {
+        this.oriented = oriented;
     }
 
     public static class Node<E> {
@@ -46,19 +58,37 @@ public class Graph<E> {
 
         @Override
         public String toString() {
-            return String.valueOf(value) + ": {" + neighbours.toString() + "}";
+            StringBuilder sb = new StringBuilder();
+
+            sb.append(value);
+            sb.append(": {");
+
+            for (Node<E> neighbour : neighbours) {
+                sb.append(neighbour.value);
+                sb.append(", ");
+            }
+
+            if (!neighbours.isEmpty()) {
+                sb.delete(sb.length() - 2, sb.length());
+            }
+
+            sb.append("}");
+
+            return sb.toString();
         }
     }
-    private final Set<Node> nodes = new HashSet<>();
 
     public Node<E> add(E value) {
-        Node<E> node = new Node(value);
+        Node<E> node = new Node<>(value);
         nodes.add(node);
         return node;
     }
 
     public void link(Node<E> source, Node<E> target) {
         source.add(target);
-    }
 
+        if (!oriented) {
+            target.add(source);
+        }
+    }
 }
